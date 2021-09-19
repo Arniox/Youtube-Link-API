1b4d1a100128438b5b72401fdc9e69ce2b6d5572
Arniox <nikkdiehl@gmail.com>
2021-09-20 00:20:17 +1200

    Maxed the playlist limit to 500 instead of Infinity so it should never time out

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ playListPromise = (search) => {
     if (ytpl.validateID(search)) {
         //Return a promise for the playlist
         return new Promise((resolve, reject) => {
-            ytpl(search, { limit: Infinity }).then((playList) => {
+            ytpl(search, { limit: /*Infinity*/500 }).then((playList) => {
                 //Resolve the playlist maped with links to an array
                 resolve(playList.items
                     .filter(vid => !vid.isLive)
